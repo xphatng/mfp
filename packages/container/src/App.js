99e40231138e167798cfe5f64d893cb835b821af
Phat Nguyen <phat.nguyen@opswat.com>
2023-11-26 17:58:15 +0700

    Remove angular app

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import { mount, mounttest } from 'sales/SalesBoot';
+// import { mount, mounttest } from 'sales/SalesBoot';
 import MarketingApp from './components/marketingApp';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
@@ -13,9 +13,9 @@ export default () => {
 	});
 	return (
 		<div style={{ display: 'flex' }}>
-			<div style={{ width: '50%' }}>
+			{/* <div style={{ width: '50%' }}>
 				<app-root></app-root>
-			</div>
+			</div> */}
 			<div style={{ width: '50%' }}>
 				<MarketingApp />
 			</div>
