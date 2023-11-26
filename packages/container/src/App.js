c1829476abf37cc744679d9739484139dfd4ada8
Phat Nguyen <phat.nguyen@opswat.com>
2023-11-26 18:35:17 +0700

    force run

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,13 +3,12 @@ import MarketingApp from './components/marketingApp';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import { StylesProvider } from '@material-ui/core/styles';
 import React, { useRef, useEffect } from 'react';
-console.log(mounttest);
 
 export default () => {
 	const ref = useRef(null);
 
 	useEffect(() => {
-		mount();
+		//mount();
 	});
 	return (
 		<div style={{ display: 'flex' }}>
